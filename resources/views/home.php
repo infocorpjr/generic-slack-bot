ab798c304175c6c79419a190cb88e2171e816ad1
Marcus Goldschmidt Oliveira <marcus.golds@hotmail.com>
2019-04-18 16:27:44 -0400

    Fist commit

---
+++ resources/views/home.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Niramit" rel="stylesheet">
+    <link rel="shortcut icon" href="/assets/favicon.png">
+    <title>API - INFOCORP</title>
+    <style>
+        html, body {
+            font-family: Niramit, sans-serif;
+            background-color: #898989;
+            text-align: center;
+            font-size: 20px;
+        }
+
+        .container {
+            margin-top: 20%;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>API INFOCORP</h1>
+    <h2>Error: 404</h2>
+    <h3>Se não é um json, então você errou XD.</h3>
+</div>
+</body>
+</html>
